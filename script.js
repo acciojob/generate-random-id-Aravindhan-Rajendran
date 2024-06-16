2cfd08ff509998f978e7c79221e58d05b0ef27fa
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-16 23:20:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function makeid(l) {
   // write your code here
+	let res="";
+	let characters='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for ( let i = 0; i< l; i++ ) {
+        res += characters.charAt(Math.floor(Math.random() * characters.Length));
+    }
+return res;
 }
 
-// Do not change the code below.
+// Do not change the code below
 const l = prompt("Enter a number.");
 alert(makeid(l));
